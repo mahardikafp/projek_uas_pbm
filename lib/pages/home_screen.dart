73b3750340fee7326c4ef78aeae28488372ca1dc
mahardikafp <71873848+mahardikafp@users.noreply.github.com>
2021-11-01 16:12:22 +0700

    Update home_screen.dart

--- lib/pages/home_screen.dart
+++ lib/pages/home_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/services.dart';
 import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:projek_uas/main.dart';
+import 'package:projek_uas/pages/yasin.dart';
 import 'count.dart';
 import 'detail.dart';
 
@@ -30,140 +31,141 @@ class _HomeScreenState extends State<HomeScreen> {
 
   @override
   Widget build(BuildContext context) {
-    getjus30();
-    return Scaffold(
-      backgroundColor: Colors.purple,
-      appBar: AppBar(
-        title: Text('Projek'),
-        backgroundColor: Colors.purple,
-      ),
-      floatingActionButton: _selectedIndex == 1
-          ? FloatingActionButton(
+    return FutureBuilder(
+        future: getjus30(),
+        builder: (context, data) {
+          return Scaffold(
+            backgroundColor: Colors.white,
+            appBar: AppBar(
+              title: Text('Projek'),
               backgroundColor: Colors.green,
-              onPressed: incrementCounter,
-              tooltip: 'increment',
-              child: Icon(Icons.add),
-            )
-          : new Visibility(
-              visible: false,
-              child: new FloatingActionButton(
-                onPressed: () {},
-                tooltip: 'Increment',
-              ),
             ),
-      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
-      body: ListView(
-        children: [
-          if (_selectedIndex == 1)
-            Count()
-          else if (_selectedIndex == 0)
-            Column(
-              children: [
-                for (int i = 0; i < jus30.length; i++)
-                  ListTile(
-                    onTap: () {
-                      getSurat(jus30[i]['id']);
-                      // print(isiSurat['data']);
-                      Navigator.of(context)
-                          .push(MaterialPageRoute(builder: (context) {
-                        return DetailSurat();
-                      }));
-                    },
-                    title: Text(
-                      jus30[i]["surat_name"].toString(),
-                      style:
-                          TextStyle(fontWeight: FontWeight.w500, fontSize: 20),
-                    ),
-                    leading: CircleAvatar(
-                      child: Text(jus30[i]["id"].toString()),
-                    ),
-                    subtitle: Text(jus30[i]["surat_text"].toString(),
-                        style: TextStyle(
-                            fontWeight: FontWeight.w800, fontSize: 15)),
-                    trailing: CircleAvatar(
-                      radius: 60,
-                      child: Text(jus30[i]["count_ayat"].toString() + ' ayat'),
-                    ),
+            floatingActionButton: _selectedIndex == 1
+                ? FloatingActionButton(
+                    backgroundColor: Colors.green,
+                    onPressed: incrementCounter,
+                    tooltip: 'increment',
+                    child: Icon(Icons.add),
                   )
+                : new Visibility(
+                    visible: false,
+                    child: new FloatingActionButton(
+                      onPressed: () {},
+                      tooltip: 'Increment',
+                    ),
+                  ),
+            floatingActionButtonLocation:
+                FloatingActionButtonLocation.centerFloat,
+            body: ListView(
+              children: [
+                if (_selectedIndex == 1)
+                  Count()
+                else if (_selectedIndex == 0)
+                  Column(
+                    children: [
+                      for (int i = 0; i < jus30.length; i++)
+                        ListTile(
+                          onTap: () {
+                            Navigator.of(context)
+                                .push(MaterialPageRoute(builder: (context) {
+                              return DetailSurat(
+                                  jus30[i]['id'], jus30[i]['surat_name']);
+                            }));
+                          },
+                          title: Text(
+                            jus30[i]["surat_name"].toString(),
+                            style: TextStyle(
+                                fontWeight: FontWeight.w500, fontSize: 20),
+                          ),
+                          leading: CircleAvatar(
+                            backgroundColor: Colors.green,
+                            child: Text(
+                              jus30[i]["id"].toString(),
+                              style: TextStyle(color: Colors.white),
+                            ),
+                          ),
+                          subtitle: Text(jus30[i]["surat_text"].toString(),
+                              style: TextStyle(
+                                  fontWeight: FontWeight.w800, fontSize: 15)),
+                          trailing: CircleAvatar(
+                            backgroundColor: Colors.lightBlue,
+                            radius: 60,
+                            child: Text(
+                                jus30[i]["count_ayat"].toString() + ' ayat',
+                                style: TextStyle(color: Colors.white)),
+                          ),
+                        )
+                    ],
+                  )
+                //   ),
+                // Container(
+                //   margin: EdgeInsets.symmetric(vertical: 20, horizontal: 10),
+                //   color: Colors.blue,
+                //   width: double.infinity,
+                //   height: 400,
+                //   child: Text(jus30[0]['surat_text'].toString()),
+                // ),
               ],
-            )
-          //   ),
-          // Container(
-          //   margin: EdgeInsets.symmetric(vertical: 20, horizontal: 10),
-          //   color: Colors.blue,
-          //   width: double.infinity,
-          //   height: 400,
-          //   child: Text(jus30[0]['surat_text'].toString()),
-          // ),
-        ],
-      ),
-      bottomNavigationBar: Container(
-        height: 50,
-        margin: EdgeInsets.all(10),
-        decoration: BoxDecoration(
-          color: Colors.black,
-          borderRadius: BorderRadius.only(
-              topLeft: Radius.circular(40), topRight: Radius.circular(40)),
-        ),
-        child: BottomNavigationBar(
-          items: const <BottomNavigationBarItem>[
-            BottomNavigationBarItem(
-              icon: Icon(Icons.home, size: 15),
-              label: 'Home',
             ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.other_houses, size: 15),
-              label: 'Dzikir Counter',
-            ),
-          ],
-          currentIndex: _selectedIndex,
-          selectedItemColor: Colors.amber[800],
-          onTap: _onItemTapped,
-        ),
-      ),
-      drawer: Drawer(
-        child: ListView(
-          padding: EdgeInsets.zero,
-          children: [
-            DrawerHeader(
+            bottomNavigationBar: Container(
+              height: 50,
+              margin: EdgeInsets.all(10),
               decoration: BoxDecoration(
-                  color: Colors.deepOrangeAccent,
-                  image: DecorationImage(
-                      image: AssetImage("images/juzAmma.png"),
-                      fit: BoxFit.cover)),
-              child: Text(''),
-            ),
-            ListTile(
-              title: Text(
-                'Item 1',
-                style: TextStyle(),
+                color: Colors.black,
+                borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(40),
+                    topRight: Radius.circular(40)),
               ),
-              onTap: () {
-                Navigator.pop(context);
-                ;
-              },
+              child: BottomNavigationBar(
+                items: const <BottomNavigationBarItem>[
+                  BottomNavigationBarItem(
+                    icon: Icon(Icons.home, size: 15),
+                    label: 'Home',
+                  ),
+                  BottomNavigationBarItem(
+                    icon: Icon(Icons.other_houses, size: 15),
+                    label: 'Dzikir Counter',
+                  ),
+                ],
+                currentIndex: _selectedIndex,
+                selectedItemColor: Colors.amber[800],
+                onTap: _onItemTapped,
+              ),
             ),
-            ListTile(
-              title: Text('Item 2'),
-              onTap: () {
-                Navigator.pop(context);
-              },
+            drawer: Drawer(
+              child: ListView(
+                padding: EdgeInsets.zero,
+                children: [
+                  DrawerHeader(
+                    decoration: BoxDecoration(
+                        color: Colors.deepOrangeAccent,
+                        image: DecorationImage(
+                            image: AssetImage("images/juzAmma.png"),
+                            fit: BoxFit.cover)),
+                    child: Text(''),
+                  ),
+                  ListTile(
+                    title: Text('Item 3'),
+                    onTap: () {
+                      Navigator.pop(context);
+                    },
+                  ),
+                  ListTile(
+                    title: Text('Item 3'),
+                    onTap: () {
+                      Navigator.pop(context);
+                    },
+                  ),
+                  ListTile(
+                    title: Text('Item 4'),
+                    onTap: () {
+                      Navigator.pop(context);
+                    },
+                  ),
+                ],
+              ),
             ),
-            ListTile(
-              title: Text('Item 3'),
-              onTap: () {
-                Navigator.pop(context);
-              },
-            ),
-            ListTile(
-              title: Text('Item 4'),
-              onTap: () {
-                Navigator.pop(context);
-              },
-            ),
-          ],
-        ),
-      ),
-    );
+          );
+        });
   }
 }
