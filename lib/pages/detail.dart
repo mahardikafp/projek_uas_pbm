105cc6427e66ce77c4cfa4cbbeeca9cb2b9c9df2
mahardikafp <71873848+mahardikafp@users.noreply.github.com>
2021-11-01 16:11:23 +0700

    Update detail.dart

--- lib/pages/detail.dart
+++ lib/pages/detail.dart
@@ -4,6 +4,10 @@ import 'package:google_fonts/google_fonts.dart';
 import 'package:projek_uas/main.dart';
 
 class DetailSurat extends StatefulWidget {
+  int idSurat;
+  String namaSurat;
+  DetailSurat(this.idSurat, this.namaSurat);
+
   @override
   _DetailSuratState createState() => _DetailSuratState();
 }
@@ -11,42 +15,47 @@ class DetailSurat extends StatefulWidget {
 class _DetailSuratState extends State<DetailSurat> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("detail surat"),
-        backgroundColor: Colors.purple,
-      ),
-      body: ListView(
-        children: [
-          for (var i = 0; i < isiSurat['data'].length; i++)
-            Container(
-              margin: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
-              width: double.infinity,
-              child: Card(
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    Text(
-                      isiSurat['data'][i]['aya_text'].toString(),
-                      textAlign: TextAlign.end,
-                      style: TextStyle(fontSize: 40),
+    return FutureBuilder(
+      future: getSurat(widget.idSurat),
+      builder: (context, data) {
+        return Scaffold(
+          appBar: AppBar(
+            title: Text(widget.namaSurat),
+            backgroundColor: Colors.purple,
+          ),
+          body: ListView(
+            children: [
+              for (var i = 0; i < isiSurat['data'].length; i++)
+                Container(
+                  margin: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+                  width: double.infinity,
+                  child: Card(
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Text(
+                          isiSurat['data'][i]['aya_text'].toString(),
+                          textAlign: TextAlign.end,
+                          style: TextStyle(fontSize: 40),
+                        ),
+                        CircleAvatar(
+                          radius: 15,
+                          foregroundColor: Colors.black,
+                          child: Text(
+                            isiSurat['data'][i]['aya_number'].toString(),
+                            style: GoogleFonts.lalezar(
+                                textStyle: TextStyle(color: Colors.red)),
+                          ),
+                        ),
+                      ],
                     ),
-                    CircleAvatar(
-                      radius: 15,
-                      foregroundColor: Colors.black,
-                      child: Text(
-                        isiSurat['data'][i]['aya_number'].toString(),
-                        style: GoogleFonts.lalezar(
-                            textStyle: TextStyle(color: Colors.red)),
-                      ),
-                    ),
-                  ],
+                  ),
                 ),
-              ),
-            ),
-        ],
-      ),
+            ],
+          ),
+        );
+      },
     );
   }
 }
