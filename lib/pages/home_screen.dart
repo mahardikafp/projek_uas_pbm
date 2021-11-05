a62f04814aaab924ed482cf4d97096d1ef4b5690
mahardikafp <71873848+mahardikafp@users.noreply.github.com>
2021-11-05 14:23:41 +0700

    Update home_screen.dart

--- lib/pages/home_screen.dart
+++ lib/pages/home_screen.dart
@@ -2,7 +2,6 @@ import 'package:flutter/services.dart';
 import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:projek_uas/main.dart';
-import 'package:projek_uas/pages/yasin.dart';
 import 'count.dart';
 import 'detail.dart';
 
@@ -14,6 +13,17 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  bool isSearching = false;
+  Future<void> getjus30() async {
+    final String response = await rootBundle.loadString("assets/juz30.json");
+    final data = await json.decode(response);
+    jus30 = data['data'];
+  }
+
+  void _getjus30(value) {
+    print(value);
+  }
+
   void incrementCounter() {
     setState(() {
       mainCounter++;
@@ -37,7 +47,40 @@ class _HomeScreenState extends State<HomeScreen> {
           return Scaffold(
             backgroundColor: Colors.white,
             appBar: AppBar(
-              title: Text('Projek'),
+              title: !isSearching
+                  ? Text('Juz Amma')
+                  : TextField(
+                      onSubmitted: (value) {
+                        setState(() {
+                          searchSeasion = value.toString();
+                        });
+                      },
+                      style: TextStyle(color: Colors.white),
+                      decoration: InputDecoration(
+                          icon: Icon(Icons.search_rounded, color: Colors.white),
+                          hintText: "search surat",
+                          hintStyle: TextStyle(color: Colors.white)),
+                    ),
+              actions: <Widget>[
+                isSearching
+                    ? IconButton(
+                        icon: Icon(Icons.cancel),
+                        onPressed: () {
+                          setState(() {
+                            this.isSearching = !this.isSearching;
+                            searchSeasion = "";
+                          });
+                        },
+                      )
+                    : IconButton(
+                        icon: Icon(Icons.search_rounded),
+                        onPressed: () {
+                          setState(() {
+                            this.isSearching = !this.isSearching;
+                          });
+                        },
+                      )
+              ],
               backgroundColor: Colors.green,
             ),
             floatingActionButton: _selectedIndex == 1
@@ -51,7 +94,7 @@ class _HomeScreenState extends State<HomeScreen> {
                     visible: false,
                     child: new FloatingActionButton(
                       onPressed: () {},
-                      tooltip: 'Increment',
+                      tooltip: 'increment',
                     ),
                   ),
             floatingActionButtonLocation:
@@ -64,68 +107,69 @@ class _HomeScreenState extends State<HomeScreen> {
                   Column(
                     children: [
                       for (int i = 0; i < jus30.length; i++)
-                        ListTile(
-                          onTap: () {
-                            Navigator.of(context)
-                                .push(MaterialPageRoute(builder: (context) {
-                              return DetailSurat(
-                                  jus30[i]['id'], jus30[i]['surat_name']);
-                            }));
-                          },
-                          title: Text(
-                            jus30[i]["surat_name"].toString(),
-                            style: TextStyle(
-                                fontWeight: FontWeight.w500, fontSize: 20),
-                          ),
-                          leading: CircleAvatar(
-                            backgroundColor: Colors.green,
-                            child: Text(
-                              jus30[i]["id"].toString(),
-                              style: TextStyle(color: Colors.white),
-                            ),
-                          ),
-                          subtitle: Text(jus30[i]["surat_text"].toString(),
-                              style: TextStyle(
-                                  fontWeight: FontWeight.w800, fontSize: 15)),
-                          trailing: CircleAvatar(
-                            backgroundColor: Colors.lightBlue,
-                            radius: 60,
-                            child: Text(
-                                jus30[i]["count_ayat"].toString() + ' ayat',
-                                style: TextStyle(color: Colors.white)),
-                          ),
-                        )
+                        (jus30[i]['surat_name'].toString())
+                                .contains(searchSeasion)
+                            ? ListTile(
+                                onTap: () {
+                                  Navigator.of(context).push(
+                                      MaterialPageRoute(builder: (context) {
+                                    return DetailSurat(
+                                        jus30[i]['id'], jus30[i]['surat_name']);
+                                  }));
+                                },
+                                title: Text(
+                                  jus30[i]["surat_name"].toString(),
+                                  style: TextStyle(
+                                      fontWeight: FontWeight.w500,
+                                      fontSize: 20),
+                                ),
+                                leading: CircleAvatar(
+                                  backgroundColor: Colors.green,
+                                  child: Text(
+                                    jus30[i]["id"].toString(),
+                                    style: TextStyle(color: Colors.white),
+                                  ),
+                                ),
+                                subtitle: Text(
+                                    jus30[i]["surat_text"].toString(),
+                                    style: TextStyle(
+                                        fontWeight: FontWeight.w800,
+                                        fontSize: 15)),
+                                trailing: CircleAvatar(
+                                  backgroundColor: Colors.lightBlue,
+                                  radius: 24,
+                                  child: Text(
+                                      jus30[i]["count_ayat"].toString() +
+                                          ' ayat',
+                                      style: TextStyle(
+                                          color: Colors.white, fontSize: 13.3)),
+                                ),
+                              )
+                            : Container()
                     ],
                   )
-                //   ),
-                // Container(
-                //   margin: EdgeInsets.symmetric(vertical: 20, horizontal: 10),
-                //   color: Colors.blue,
-                //   width: double.infinity,
-                //   height: 400,
-                //   child: Text(jus30[0]['surat_text'].toString()),
-                // ),
               ],
             ),
-            bottomNavigationBar: Container(
-              height: 50,
-              margin: EdgeInsets.all(10),
-              decoration: BoxDecoration(
-                color: Colors.black,
-                borderRadius: BorderRadius.only(
-                    topLeft: Radius.circular(40),
-                    topRight: Radius.circular(40)),
+            bottomNavigationBar: ClipRRect(
+              borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(30),
+                topRight: Radius.circular(30),
               ),
               child: BottomNavigationBar(
+                backgroundColor: Colors.green,
                 items: const <BottomNavigationBarItem>[
                   BottomNavigationBarItem(
-                    icon: Icon(Icons.home, size: 15),
+                    icon: Icon(Icons.home, size: 20),
                     label: 'Home',
                   ),
                   BottomNavigationBarItem(
-                    icon: Icon(Icons.other_houses, size: 15),
+                    icon: Icon(Icons.other_houses, size: 20),
                     label: 'Dzikir Counter',
                   ),
+                  // BottomNavigationBarItem(
+                  //   icon: Icon(Icons.search_rounded, size: 20),
+                  //   label: 'search',
+                  // ),
                 ],
                 currentIndex: _selectedIndex,
                 selectedItemColor: Colors.amber[800],
@@ -145,10 +189,8 @@ class _HomeScreenState extends State<HomeScreen> {
                     child: Text(''),
                   ),
                   ListTile(
-                    title: Text('Item 3'),
-                    onTap: () {
-                      Navigator.pop(context);
-                    },
+                    title: Text('Yā Sīn'),
+                    onTap: () {},
                   ),
                   ListTile(
                     title: Text('Item 3'),
