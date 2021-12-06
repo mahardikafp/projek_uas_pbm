606edddef88f2c4f9ea7d4669529343e21f6a036
mahardikafp <71873848+mahardikafp@users.noreply.github.com>
2021-12-06 12:05:35 +0700

    Update detail.dart

--- lib/pages/detail.dart
+++ lib/pages/detail.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:projek_uas/main.dart';
+import 'package:juz__amma/main.dart';
 
 class DetailSurat extends StatefulWidget {
   int idSurat;
